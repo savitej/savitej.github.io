fcdaa97eef4387099d15e19fc232c480a6113b56
savitej <32020545+savitej@users.noreply.github.com>
2020-08-15 18:27:41 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const testlink = document.querySelector('.header .nav-bar .nav-list .testlink');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header = document.querySelector('.header.container');
 
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
+testlink.addEventListener('click', () => {
+	testlink.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
 });
 
@@ -19,7 +19,7 @@ document.addEventListener('scroll', () => {
 
 menu_item.forEach((item) => {
 	item.addEventListener('click', () => {
-		hamburger.classList.toggle('active');
+		testlink.classList.toggle('active');
 		mobile_menu.classList.toggle('active');
 	});
 });
